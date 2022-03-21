c1ae7aa944494908366e26d3f6ded83da403c00c
Chaewony <tcd1205@naver.com>
2022-03-22 05:51:59 +0900

    Blueprint Class

--- my/Intermediate/Build/Win64/UE4Editor/Inc/my/my.init.gen.cpp
+++ my/Intermediate/Build/Win64/UE4Editor/Inc/my/my.init.gen.cpp
@@ -21,8 +21,8 @@ void EmptyLinkFunctionForGeneratedCodemy_init() {}
 				nullptr,
 				0,
 				PKG_CompiledIn | 0x00000000,
-				0x1AF0BF35,
-				0xCD3B45FA,
+				0x40917FAB,
+				0xC4652A7D,
 				METADATA_PARAMS(nullptr, 0)
 			};
 			UE4CodeGen_Private::ConstructUPackage(ReturnPackage, PackageParams);
